b23c627e862edbbd2c31a47cf06777434fb05f36
tcavalluzzi7656 <tcavalluzzi7656@bths.edu>
2018-11-21 15:05:36 -0500

    vcgvcgvdsf

--- src/CodeWordChecker.java
+++ src/CodeWordChecker.java
@@ -1,21 +1,29 @@
-public class CodeWordChecker {
+public class CodeWordChecker implements StringChecker {
     private int min;
     private int max;
-    private  String not;
-    public CodeWordChecker(min,max,not)
+    private String not;
+    private String str;
+
+    public CodeWordChecker(int min,int max,String not)
     {
+
         this.min=min;
         this.max=max;
         this.not=not;
     }
-    public CodeWordChecker(not) {
+    public CodeWordChecker(String not) {
         this.min = 6;
         this.max = 20;
         this.not = not;
+
     }
 
-    public static boolean isValid(String str)
+    public boolean isValid(String str)
     {
-        if(str.indexOf(not))
+        if(((str.indexOf(not))!=-1)||(str.length()<min)||(str.length()>max))
+        {
+            return false;
+        }
+        return true;
     }
 }
